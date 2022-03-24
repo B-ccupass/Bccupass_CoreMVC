cea8f665243255dd81db35ca01c0776d0a02a6a1
郭家秀 <a880318@gmail.com>
2022-03-24 17:07:07 +0800

    加入專案檔案。

---
+++ Bccupass_CoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bccupass_CoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
