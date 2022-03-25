e7c9c6a2d3ba80fe98aa43a26d8fd77af32ea53b
Song <lkksppsss@gmail.com>
2022-03-25 14:04:53 +0800

    加入DTO和ViewModel資料夾 新增ActivityCardDto和ActivityCardViewModel 訂正一些檔案的using 命名空間路徑

---
+++ Bccupass_CoreMVC/Models/DTO/Activity/ActivityCardDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bccupass_CoreMVC.Models.DTO.Activity
+{
+    public class ActivityCardDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int? City { get; set; }
+        public int ActivityPrimaryThemeId { get; set; }
+    }
+}
---
+++ Bccupass_CoreMVC/Models/ViewModel/ActivityCard/ActivityCardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bccupass_CoreMVC.Models.ViewModel.ActivityCard
+{
+    public class ActivityCardViewModel
+    {
+        public IEnumerable<ActivityData> ActivityList { get; set; }
+        public class ActivityData
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Image { get; set; }
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+            public int? City { get; set; }
+            public int ActivityPrimaryThemeId { get; set; }
+        }
+    }
+}
