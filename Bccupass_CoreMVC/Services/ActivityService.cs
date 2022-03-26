a0c5885e0baf26b602897de222e0e6f14fbb7e19
郭家秀 <a880318@gmail.com>
2022-03-26 23:32:20 +0800

    fix: activityCards partialview

--- Bccupass_CoreMVC/Services/ActivityService.cs
+++ Bccupass_CoreMVC/Services/ActivityService.cs
@@ -132,17 +132,23 @@ namespace Bccupass_CoreMVC.Services
 
         public IEnumerable<ActivityCardDto> GetNewestActivity()
         {
-            var target = _context.GetAll<Activity>().Where(x => x.ActivityState == 1).OrderByDescending(x => x.CreateTime).Take(6);//抓最新的前6個(活動的篩選)
+            //var target = _context.GetAll<Activity>().Where(x => x.ActivityState == 1).OrderByDescending(x => x.CreateTime).Take(6);//抓最新的前6個(活動的篩選)
+            var target = _context.GetAll<Activity>().Where(x => x.ActivityState == 1).OrderByDescending(x => x.CreateTime);//抓最新的前6個(活動的篩選)
 
             return ActivityCardDtoResult(target);
         }
 
         public IEnumerable<ActivityCardDto> GetChosenActivity()
         {
-            var userFollowing = _context.GetAll<UserFollowOrganizer>().GroupBy(x => x.OrganizerId).OrderByDescending(x => x.Select(s => s.UserId).Count()).Take(4);//追隨數前4名的主辦
-            var ordId = userFollowing.Select(group => new { OrganizerId = group.Key }).ToList();
+            //var userFollowing = _context.GetAll<UserFollowOrganizer>().GroupBy(x => x.OrganizerId).OrderByDescending(x => x.Select(s => s.UserId).Count()).Take(4);//追隨數前4名的主辦
+            //var ordId = userFollowing.Select(group => new { OrganizerId = group.Key }).ToList();
 
-            var target = _context.GetAll<Activity>().Where(x => x.OrganizerId == ordId[0].OrganizerId || x.OrganizerId == ordId[1].OrganizerId || x.OrganizerId == ordId[2].OrganizerId || x.OrganizerId == ordId[3].OrganizerId).OrderByDescending(x => x.CreateTime).Take(6);
+            //var target = _context.GetAll<Activity>().Where(x => x.OrganizerId == ordId[0].OrganizerId || x.OrganizerId == ordId[1].OrganizerId || x.OrganizerId == ordId[2].OrganizerId || x.OrganizerId == ordId[3].OrganizerId).OrderByDescending(x => x.CreateTime).Take(6);
+
+            //return ActivityCardDtoResult(target);
+
+            // 測試
+            var target = _context.GetAll<Activity>().Where(x => x.ActivityState == 1).OrderByDescending(x => x.CreateTime);//抓最新的前6個(活動的篩選)
 
             return ActivityCardDtoResult(target);
         }
