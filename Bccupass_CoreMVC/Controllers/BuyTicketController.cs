1dce6f70c32e2fefac9df9b934ed4ef0f3df93cd
Song <lkksppsss@gmail.com>
2022-03-28 22:21:57 +0800

    將完成的切版加進MVC

---
+++ Bccupass_CoreMVC/Controllers/BuyTicketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bccupass_CoreMVC.Controllers
+{
+    public class BuyTicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
