b3a3f04e6814ef7610dacc3d03106fa98412b9c1
郭家秀 <a880318@gmail.com>
2022-03-29 15:55:50 +0800

    fix: command

--- Bccupass_CoreMVC/Models/DBEntity/BccupassDBContext.cs
+++ Bccupass_CoreMVC/Models/DBEntity/BccupassDBContext.cs
@@ -38,14 +38,14 @@ namespace Bccupass_CoreMVC.Models.DBEntity
         public virtual DbSet<UserFavorite> UserFavorites { get; set; }
         public virtual DbSet<UserFollowOrganizer> UserFollowOrganizers { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:bs-2021-winter-bccupass.database.windows.net,1433;Initial Catalog=BccupassDB;Persist Security Info=False;User ID=bs;Password=P@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=tcp:bs-2021-winter-bccupass.database.windows.net,1433;Initial Catalog=BccupassDB;Persist Security Info=False;User ID=bs;Password=P@ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
