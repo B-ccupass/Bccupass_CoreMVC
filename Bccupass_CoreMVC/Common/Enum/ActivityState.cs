1468bd4dc84cf0b17854efbe137c132061584aa9
LunAhEric <luochen9913@gmail.com>
2022-03-29 22:10:26 +0800

    add ActivityState Enum

---
+++ Bccupass_CoreMVC/Common/Enum/ActivityState.cs
@@ -0,0 +1,26 @@
+﻿namespace Bccupass_CoreMVC.Common.Enum
+{
+    public enum ActivityState
+    {
+        /// <summary>
+        /// 草稿
+        /// </summary>
+        Draft = 0,
+        /// <summary>
+        /// 已上架
+        /// </summary>
+        Available = 1,
+        /// <summary>
+        /// 已下架
+        /// </summary>
+        Removed = 2,
+        /// <summary>
+        /// 已結束
+        /// </summary>
+        Closed = 3,
+        /// <summary>
+        /// 已刪除
+        /// </summary>
+        IDelete = 4,
+    }
+}
