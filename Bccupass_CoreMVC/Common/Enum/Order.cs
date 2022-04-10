f5195949d4cb253d27f7f54b7a663935023aa5f2
Leo Chang <ja42022@gmail.com>
2022-04-10 11:01:45 +0800

    新增Enum

---
+++ Bccupass_CoreMVC/Common/Enum/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Bccupass_CoreMVC.Common.Enums
+{
+    public class Order
+    {
+        public enum OrderState
+        {
+            [Description("未付款")]
+            NotPaid,
+            [Description("已付款")]
+            Paid,
+            [Description("已取消")]
+            Cancel,
+            [Description("已退票")]
+            Refund
+        }
+    }
+}
