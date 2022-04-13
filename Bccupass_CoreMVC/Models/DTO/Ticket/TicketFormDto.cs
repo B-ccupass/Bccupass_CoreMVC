d95f6e27c43ca2b93807c8d7568825d32301fdc8
郭家秀 <a880318@gmail.com>
2022-04-13 10:11:24 +0800

    feat: buyer form

--- Bccupass_CoreMVC/Models/DTO/Ticket/TicketFormDto.cs
+++ Bccupass_CoreMVC/Models/DTO/Ticket/TicketFormDto.cs
@@ -9,15 +9,15 @@
         public bool Address { get; set; }
         public bool Gender { get; set; }
         public bool Age { get; set; }
-        public bool Hobby { get; set; }
-        public bool MaritalStatus { get; set; }
-        public bool Industry { get; set; }
-        public bool Department { get; set; }
+        //public bool Hobby { get; set; }
+        //public bool MaritalStatus { get; set; }
+        //public bool Industry { get; set; }
+        //public bool Department { get; set; }
         public bool IdNumber { get; set; }
-        public bool Fax { get; set; }
-        public bool EducationLevel { get; set; }
-        public bool DiningNeeds { get; set; }
-        public bool CompanyName { get; set; }
-        public bool JobTitle { get; set; }
+        //public bool Fax { get; set; }
+        //public bool EducationLevel { get; set; }
+        //public bool DiningNeeds { get; set; }
+        //public bool CompanyName { get; set; }
+        //public bool JobTitle { get; set; }
     }
 }
